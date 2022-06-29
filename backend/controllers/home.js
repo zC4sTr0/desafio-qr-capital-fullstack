e72a0abbed9e715375c54fd32079596f53464eac
Gabriel Castro <C4sTr0@live.com>
2022-06-28 21:08:35 -0300

    build(backend): node modules and npm configuration Added package.json to the project with the following dependencies: node, mysql2, express and nodemon. Test homepage included

--- backend/controllers/home.js
+++ backend/controllers/home.js
@@ -1,14 +1,6 @@
 const path = require('path');
 
 
-exports.getHomePage = (req, res, next) => {
-    const all_contacts = Contato.getContacts();
-    all_contacts.then(([rows, fieldData]) => {
-        res.render('home', {
-            contact_list: rows,
-            pageTitle: 'Home',
-            path: '/',
-        });
-    }).catch((err) => console.log(err));
-
+exports.getHomePage = (req, res) => {
+    res.send('<h1>Hello World from Home Page</h1>');
 };
